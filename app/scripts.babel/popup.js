ecf96536ad46f41a05e5699a96316e9a4a91f6b7
daVinciBot1495 <cdegranville@rnsoftwareok.com>
2017-03-05 16:58:35 -0800

    Resolves #1 - Provide a push back and pop front menu.

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+function pushBack() {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        console.log('pushBack: ' + url);
+    });
+}
+
+function popFront() {
+    console.log('popFront');
+}
+
+document.getElementById('pushBack').addEventListener('click', pushBack);
+document.getElementById('popFront').addEventListener('click', popFront);
