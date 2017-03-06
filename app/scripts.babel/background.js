8168bde35fa1b10d45281c77d913ef8fd3394cee
daVinciBot1495 <cdegranville@rnsoftwareok.com>
2017-03-05 19:19:46 -0800

    Resolves #2: Implement in memory article queue.

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,6 +4,43 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+function InMemoryArticleQueue() {
+    var queue = [];
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+    return {
+        size: function() {
+            return queue.length;
+        },
+
+        pushBack: function(url) {
+            if (url) {
+                queue.push(url);
+            }
+        },
+
+        popFront: function() {
+            return queue.splice(0, 1)[0];
+        }
+    };
+}
+
+var artiq = new InMemoryArticleQueue();
+
+function refreshBadgeText() {
+    var queueSize = artiq.size();
+    console.log('size: ' + queueSize);
+    chrome.browserAction.setBadgeText({text: queueSize.toString()});
+}
+
+function pushBack(url) {
+    artiq.pushBack(url);
+    refreshBadgeText();
+}
+
+function popFront() {
+    var url = artiq.popFront();
+    refreshBadgeText();
+    return url;
+}
+
+refreshBadgeText();
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -4,11 +4,20 @@ function pushBack() {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
         console.log('pushBack: ' + url);
+        chrome.extension.getBackgroundPage().pushBack(url);
     });
 }
 
 function popFront() {
-    console.log('popFront');
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var tabId = tabs[0].id;
+        var url = chrome.extension.getBackgroundPage().popFront();
+        console.log('popFront: ' + url);
+
+        if (url) {
+            chrome.tabs.update(tabId, {url: url});
+        }
+    });
 }
 
 document.getElementById('pushBack').addEventListener('click', pushBack);
